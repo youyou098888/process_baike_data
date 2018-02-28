05bfad4deb7b58d37b4d45e4e29d82d594ea8076
Yang Yu 于洋 <yang.yu@MBC02VP2AGG8WN.local>
2018-02-28 19:30:32 +0800

    remove relation constraint

--- main_entity_recog.py
+++ main_entity_recog.py
@@ -98,7 +98,8 @@ if __name__ == '__main__':
             
             for idx, obj in enumerate(info):
                 attr, entity2 = obj
-                if attr == 'BaiduCARD': # only extract BaiduCard relation
+                if 1:
+                # if attr == 'BaiduCARD': # only extract BaiduCard relation
                     possile_answers.append({'pid': pid, 'answer': entity2})
                     fh.write('---------------------------------------------\n')
                     fh.write('<subject id=' + str(qid + 1) + '-' + str(rank) + '>\t')
